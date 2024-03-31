557a921fa6dc28324385a37cefdba020175875b8
Olena457 <olenademianenko42@gmail.com>
2024-03-31 20:59:41 +0300

    added code timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -14,6 +14,7 @@ const days = document.querySelector('span[data-days]');
 
 btnStart.disabled = true;
 let userSelectedDate;
+let timer;
 
 const options = {
   enableTime: true,
@@ -23,13 +24,16 @@ const options = {
 
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
-      iziToast.info('Please choose a date in the future');
+      iziToast.show({
+        title: 'Hey',
+        message: 'Please choose a date in the future',
+      });
       btnStart.disabled = true;
     } else {
-      btnStart.disabled = false;
       userSelectedDate = selectedDates[0];
+      btnStart.disabled = false;
+      console.log(selectedDates[0]);
     }
-    console.log(selectedDates[0]);
   },
 };
 
@@ -40,46 +44,41 @@ function addLeadingZero(value) {
   const formattedValue = stringValue.padStart(2, '0');
   return formattedValue;
 }
-let timer;
-// let timeDecrease;
-let objectTime;
 
 btnStart.addEventListener('click', () => {
   timer = setInterval(() => {
-    const timeDecrease = userSelectedDate - new Date();
+    const timeDecrease = new Date(text.value) - new Date();
     btnStart.disabled = true;
     if (timeDecrease >= 0) {
-      objectTime = convertMs(timeDecrease);
+      let objectTime = convertMs(timeDecrease);
       days.textContent = addLeadingZero(objectTime.days);
       hours.textContent = addLeadingZero(objectTime.hours);
       minutes.textContent = addLeadingZero(objectTime.minutes);
       seconds.textContent = addLeadingZero(objectTime.seconds);
-      if (!timer) {
-        iziToast.info('Countdown is over');
-        clearInterval(timer);
-        timerHtml.style.display = 'none';
-      }
+    } else {
+      clearInterval(timer);
+      iziToast.show({
+        title: 'Hey',
+        message: 'Countdown is over',
+      });
+      timerHtml.style.display = 'none';
     }
   }, 1000);
 });
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
+
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
