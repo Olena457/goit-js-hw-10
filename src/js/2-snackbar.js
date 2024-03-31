418d41ac21be50e4dadce380308f8b517faa4d4c
Olena457 <olenademianenko42@gmail.com>
2024-04-01 01:31:53 +0300

    added code 2hw

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,36 @@
+// Описаний у документації
 import iziToast from 'izitoast';
-
+// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventlistener('submit', function (ev) {
+  ev.preventDefault();
+  const inputDelay = doccument.querySelector("[name='delay']");
+  const inputState = doccument.querySelector("[name='state']");
+  const delay = parseInt(inputDelay.value);
+  let resolvedDelay;
+  let rejectedDelay;
+  const creatinPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputState.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  creatinPromise
+    .then(rejectedDelay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(rejectedDelay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
