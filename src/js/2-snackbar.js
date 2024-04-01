f39a9e7b22b6c719acdc435d16d2108e0201a17f
Olena457 <olenademianenko42@gmail.com>
2024-04-01 10:16:38 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,34 +3,36 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-document.querySelector('.form').addEventlistener('submit', function (ev) {
-  ev.preventDefault();
-  const inputDelay = doccument.querySelector("[name='delay']");
-  const inputState = doccument.querySelector("[name='state']");
-  const delay = parseInt(inputDelay.value);
-  let resolvedDelay;
-  let rejectedDelay;
-  const creatinPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (inputState.value === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector("[name='delay']");
+const inputState = document.querySelector("[name='state']");
+const delay = parseInt(inputDelay.value);
+// const submit = document.querySelector('submit');
+form.addEventlistener('submit', creatinPromise);
+let resolvedDelay;
+let rejectedDelay;
 
-  creatinPromise
-    .then(rejectedDelay => {
-      iziToast.success({
-        title: 'Success',
-        message: `✅ Fulfilled promise in ${delay}ms`,
-      });
-    })
-    .catch(rejectedDelay => {
-      iziToast.error({
-        title: 'Error',
-        message: `❌ Rejected promise in ${delay}ms`,
-      });
-    });
+const creatinPromise = new Promise((resolve, reject) => {
+  ev.preventDefault();
+  setTimeout(() => {
+    if (inputState.value === 'fulfilled') {
+      resolve(delay);
+    } else {
+      reject(delay);
+    }
+  }, delay);
 });
+
+creatinPromise
+  .then(_resolvedDelay => {
+    iziToast.success({
+      title: 'Success',
+      message: `✅ Fulfilled promise in ${resolvedDelay}ms`,
+    });
+  })
+  .catch(_rejectedDelay => {
+    iziToast.error({
+      title: 'Error',
+      message: `❌ Rejected promise in ${rejectedDelay}ms`,
+    });
+  });
